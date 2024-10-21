b68dfa8c524fe1285a14a1130993a15e096f982a
Mohsen Aljiashi <58286336+Mohsen-rm@users.noreply.github.com>
2024-10-21 19:47:55 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:alarm/alarm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-//import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'screen_home.dart';
 
 
